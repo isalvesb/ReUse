6e3bce8f6acd691c2319106a0b7aaf43d39aaa75
Isa Alves <alvesisamara254@gmail.com>
2026-03-08 01:48:00 -0300

    alterações resolvidas

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "#342A2A",
+    paddingLeft: 16,
+    paddingRight: 16,
+    paddingTop: 10,
+    paddingBottom: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 8,
+    borderRadius: 20,
+  },
+
+  text: {
+    fontSize: 14,
+    fontWeight: 500,
+    fontFamily: "Roboto",
+    justifyContent: "center",
+    color: "#fff",
+  },
+});
+
+export default styles;
