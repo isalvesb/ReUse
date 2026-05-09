248cdea880954abfdbdd6582ccdf46d21d66b009
Guilherme Cunha <guicunhasou@gmail.com>
2026-05-09 19:04:57 -0300

    melhorando storage.ts

--- src/Services/Storage.ts
+++ src/Services/Storage.ts
@@ -7,3 +7,25 @@ export const salvar = async (key: string, value: string) => {
 export const buscar = async (key: string) => {
   return await AsyncStorage.getItem(key);
 };
+
+export const remover = async (key: string) => {
+  await AsyncStorage.removeItem(key);
+};
+
+export const salvarObjeto = async <T>(key: string, value: T) => {
+  await AsyncStorage.setItem(key, JSON.stringify(value));
+};
+
+export const buscarObjeto = async <T>(key: string): Promise<T | null> => {
+  const value = await AsyncStorage.getItem(key);
+
+  if (!value) {
+    return null;
+  }
+
+  return JSON.parse(value) as T;
+};
+
+export const limparCache = async (keys: string[]) => {
+  await AsyncStorage.multiRemove(keys);
+};
